45726f2fe10015c16d5093493b41b23a46b56adf
Aurojyoti <developeratbackend@gmail.com>
2024-01-28 23:18:00 +0530

    Form Validation using Form Validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,22 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    // console.log(email.current.value);
+    // console.log(password.current.value);
+    const message = checkValidData(email.current.value, password.current.value);
+
+    console.log(message);
+    setErrorMessage(message);
+  };
 
   function toggleSignIn() {
     setIsSignIn(!isSignIn);
@@ -16,7 +30,10 @@ const Login = () => {
           alt="backgroundImg"
         />
       </div>
-      <form className="absolute p-12 bg-black bg-opacity-80 w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-sm">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute p-12 bg-black bg-opacity-80 w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-sm"
+      >
         <h2 className="font-bold text-3xl py-4">
           {isSignIn ? "Sign In" : "Sign Up"}
         </h2>
@@ -28,24 +45,32 @@ const Login = () => {
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-3 my-2 w-full bg-gray-500 bg-opacity-50 rounded-sm"
         />
         <input
+          ref={password}
           type="text"
           placeholder="Password"
           className="p-3 my-2 w-full bg-gray-500 bg-opacity-50 rounded-sm"
         />
-        <button className="p-2 my-2 bg-red-700 w-full rounded-sm">
+        <p className="text-sm text-red-700 block text-center">{errorMessage}</p>
+        <button
+          onClick={handleButtonClick}
+          className="p-2 my-2 bg-red-700 w-full rounded-sm"
+        >
           {isSignIn ? "Sign In" : "Sign Up"}
         </button>
-        <a
-          href="#"
-          className="block text-center hover:underline cursor-pointer"
-        >
-          Forgot Password ?{" "}
-        </a>
+        {isSignIn ? (
+          <a
+            href="#"
+            className="block text-center hover:underline cursor-pointer"
+          >
+            Forgot Password ?{" "}
+          </a>
+        ) : null}
 
         <div className="mt-14">
           <input type="checkbox" id="rememberMe" class="h-4 w-4 mr-2" />
