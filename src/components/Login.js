ba8c551e4af57f645feb4cbd70017e85ae300e66
Aurojyoti <developeratbackend@gmail.com>
2024-01-31 07:13:12 +0530

    Sign in Page Complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,10 @@
 import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../utils/firebase";
 
 const Login = () => {
@@ -34,9 +37,25 @@ const Login = () => {
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          setErrorMessage(errorCode+ "-" +errorMessage);
+          setErrorMessage(errorCode + "-" + errorMessage);
         });
     } else {
+      // Sign In
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode+" - "+errorMessage);
+        });
     }
   };
 
