f527b25c34a78f6bbd888e999190fd2658392193
Aurojyoti <developeratbackend@gmail.com>
2024-02-11 12:16:18 +0530

    sign-in sign-out cmplt

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,21 @@ import React from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
   const navigate = useNavigate();
-  
+  const user = useSelector((store) => store.user);
+
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
-        navigate("/")
+        navigate("/");
       })
       .catch((error) => {
         navigate("/error");
       });
   };
-  
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
@@ -22,19 +24,21 @@ const Header = () => {
         src={require("../assets/Netflix_Logo_PMS.png")}
         alt="Logo"
       />
-      <div className="flex p-2 justify-between items-center w-40">
-        <img
-          alt="userIcon"
-          src="https://occ-0-2610-3662.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXz4LMjJFidX8MxhZ6qro8PBTjmHbxlaLAbk45W1DXbKsAIOwyHQPiMAuUnF1G24CLi7InJHK4Ge4jkXul1xIW49Dr5S7fc.png?r=e6e"
-          className="w-10 h-10 rounded-md"
-        />
-        <button
-          className="text-white hover:underline transition-all duration-1000"
-          onClick={handleSignOut}
-        >
-          Sign Out
-        </button>
-      </div>
+      {user && (
+        <div className="flex p-2 justify-between items-center w-40">
+          <img
+            alt="userIcon"
+            src={require("../assets/logo.png")}
+            className="w-10 h-10 rounded-md"
+          />
+          <button
+            className="text-white hover:underline transition-all duration-1000"
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -8,11 +8,15 @@ import {
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
 
   const name = useRef(null);
   const email = useRef(null);
@@ -38,9 +42,18 @@ const Login = () => {
           const user = userCredential.user;
           updateProfile(user, {
             displayName: name.current.value,
-            photoURL: "https://avatars.githubusercontent.com/u/123365412?v=4",
+            // photoURL: "https://avatars.githubusercontent.com/u/123365412?v=4",
           })
             .then(() => {
+              const {uid, email, displayName, photoURL} = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
               navigate("/browse");
             })
             .catch((error) => {
